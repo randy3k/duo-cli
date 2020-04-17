f763cbd5b053ca460f2fb9640a63266a62fd3ca8
Randy Lai <randy.cs.lai@gmail.com>
2020-04-17 16:40:36 -0700

    label may contain spaces

--- duo_export.py
+++ duo_export.py
@@ -6,6 +6,11 @@ import json
 import base64
 import sys
 
+try:
+  from urllib.parse import quote
+except ImportError:
+  from urllib import quote
+
 file_json = "response.json"
 
 with open('response.json', "r") as f:
@@ -18,7 +23,7 @@ label = response['customer_name']
 issuer = 'Duo'
 # base32 encoded hotp secret, with the padding ("=") stripped.
 secret = base64.b32encode(bytes(response['hotp_secret'], 'utf-8')).decode('utf-8').replace('=', '')
-qrdata = 'otpauth://hotp/{label}?secret={secret}&issuer={issuer}&counter={counter}'.format(label=label, secret=secret, issuer=issuer, counter=counter)
+qrdata = quote('otpauth://hotp/{label}?secret={secret}&issuer={issuer}&counter={counter}'.format(label=label, secret=secret, issuer=issuer, counter=counter))
 qrcode = pyqrcode.create(qrdata)
 print(qrcode.terminal(quiet_zone=1))
 print(qrdata)
